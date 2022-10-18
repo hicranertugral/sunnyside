5dd7a01c8d4137c61d695f9aa0bf23a64784930a
hicranertugral <hicranertugral@gmail.com>
2022-10-18 23:05:48 +0300

    sunside-hero-content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-
-import './App.css';
+import "./App.css";
+import "./utility.css";
 
 function App() {
   return (
     <div className="App">
-     <p>Sunnyside App</p>
+      <header className="hero">
+        <div className="navbar">
+          <h3 className="logo">sunnyside</h3>
+       
+          <nav>
+            <ul>
+              <li>
+                <a href="/#">About</a>
+              </li>
+              <li>
+                <a href="/#">Services</a>
+              </li>
+              <li>
+                <a href="/#">Projects</a>
+              </li>
+              <li>
+                <a href="/#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          </div>
+        <div className="content">
+          <h2>WE ARE CREATIVES</h2>
+          <h1>↓</h1>
+        </div>
+      </header>
     </div>
   );
 }
