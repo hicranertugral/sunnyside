381db46d109a14da918cbc6235ad3658fcdda702
hicranertugral <hicranertugral@gmail.com>
2022-10-19 01:35:51 +0300

    Sunnyside Navbar Components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import "./App.css";
-import "./utility.css";
+import "./App.scss";
+import "./Utility.scss";
+import Navbar from "./components/Navbar.js";
+// import HeroContent from "./components/HeroContent.js";
 
 function App() {
   return (
     <div className="App">
       <header className="hero">
-        <div className="navbar">
-          <h3 className="logo">sunnyside</h3>
-       
-          <nav>
-            <ul>
-              <li>
-                <a href="/#">About</a>
-              </li>
-              <li>
-                <a href="/#">Services</a>
-              </li>
-              <li>
-                <a href="/#">Projects</a>
-              </li>
-              <li>
-                <a href="/#">Contact</a>
-              </li>
-            </ul>
-          </nav>
-          </div>
+        <Navbar />
         <div className="content">
           <h2>WE ARE CREATIVES</h2>
           <h1>↓</h1>
         </div>
       </header>
     </div>
+
+    // <div className="App">
+    // <header className="hero">
+    //   <div className="navbar">
+    //     <h3 className="logo">sunnyside</h3>
+
+    //     <nav>
+    //       <ul>
+    //         <li>
+    //           <a href="/#">About</a>
+    //         </li>
+    //         <li>
+    //           <a href="/#">Services</a>
+    //         </li>
+    //         <li>
+    //           <a href="/#">Projects</a>
+    //         </li>
+    //         <li>
+    //           <a href="/#">Contact</a>
+    //         </li>
+    //       </ul>
+    //     </nav>
+    //     </div>
+    //   <div className="content">
+    //     <h2>WE ARE CREATIVES</h2>
+    //     <h1>↓</h1>
+    //   </div>
+    // </header>
+    // </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h3 className="logo">sunnyside</h3>
+
+      <nav>
+        {/* <a href="/#" className="hamburger">
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+        </a> */}
+        <ul>
+          <li>
+            <a href="/#">About</a>
+          </li>
+          <li>
+            <a href="/#">Services</a>
+          </li>
+          <li>
+            <a href="/#">Projects</a>
+          </li>
+          <li>
+            <a href="/#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
